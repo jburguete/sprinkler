6b4b2b5cd17e5c7253de5db3840eaf3d17b70bd9
jburguete <jburguete@eead.csic.es>
2015-12-23 16:22:09 +0100

    Correcting a bug calculating drop deformation

--- 0.1.1/generate.c
+++ 0.1.1/generate.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+#include <math.h>
+
+#define DENSITY 1.17796
+#define TENSION 0.07273984
+
+/*
+double f(double Deq, double Dmax, double v)
+{
+  double D3;
+  D3 = Dmax * Dmax * Dmax;
+  return (Deq * Deq + 0.1742 / 4. * DENSITY / TENSION * v * v * D3) * Deq - D3;
+}
+
+double Deq (double Dmax, double v)
+{
+  double d1, d2, d3;
+  unsigned int i;
+  d1 = 0.;
+  d3 = Dmax;
+  for (i = 0; i < 64; ++i)
+  {
+	  d2 = 0.5 * (d1 + d3);
+	  if (f(d2, Dmax, v) < 0.) d1 = d2; else d3 = d2;
+  }
+  return d2;
+}
+*/
+
+double Deq (double Dmax, double v)
+{
+	double kWe;
+	kWe = 0.1742 / 6. * DENSITY / TENSION * v * v * Dmax;
+	kWe = sqrt (1. + kWe * kWe * kWe);
+	return Dmax * (pow (0.5 * (1. + kWe), 1. / 3.)
+		- pow (0.5 * (kWe - 1), 1. / 3.));
+}
 
 int main()
 {
@@ -15,12 +51,20 @@ int main()
   i = 0;
   while (fscanf (file1, "%lf%lf%lf%lf%lf", x, x + 1, x + 2, x + 3, x + 4) == 5)
 	{
+	  x[3] = Deq (x[3] / 1000., x[4]);
 	  fprintf (file2, "\t<trajectory bed_level=\"-1.5\" dt=\"0.001\" "
-			   "cfl=\"0.1\" file=\"%u\">\n", ++i);
+			   "cfl=\"0.1\" file=\"o%u\">\n", ++i);
 	  fprintf (file2, "\t\t<drop diameter=\"%.14le\" velocity=\"%.14le\" "
 			   "vertical_angle=\"%lg\" x=\"%lg\" z=\"%lg\" "
 			   "drag_model=\"ovoid\"/>\n",
-			   x[3] / 1000., x[4], -x[2], x[1], x[0]);
+			   x[3], x[4], -x[2], x[1], x[0]);
+	  fprintf (file2, "\t</trajectory>\n");
+	  fprintf (file2, "\t<trajectory bed_level=\"-1.5\" dt=\"0.001\" "
+			   "cfl=\"0.1\" file=\"s%u\">\n", i);
+	  fprintf (file2, "\t\t<drop diameter=\"%.14le\" velocity=\"%.14le\" "
+			   "vertical_angle=\"%lg\" x=\"%lg\" z=\"%lg\" "
+			   "drag_model=\"sphere\"/>\n",
+			   x[3], x[4], -x[2], x[1], x[0]);
 	  fprintf (file2, "\t</trajectory>\n");
 	}
   fprintf (file2, "</invert>\n");
--- 0.1.1/generate2.c
+++ 0.1.1/generate2.c
@@ -10,39 +10,81 @@ int main()
 	fprintf (file, "set xlabel 'x (m)'\n");
 	fprintf (file, "set ylabel 'z (m)'\n");
 	fprintf (file, "f(x)=0.4403*x+6.447e-3*x**2-3.969e-3*x**3+8.113e-5*x**4\n");
-	fprintf (file, "set out '0-1.eps'\n");
+	fprintf (file, "set out 'o0-1.eps'\n");
 	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
 	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.001?$2:1/0):4 not w d,\\\n", i + 1);
-	fprintf (file, "\t'%u' u ($9<0.001?$2:1/0):4 not w d\n", i + 1);
-	fprintf (file, "set out '1-2.eps'\n");
+	  fprintf (file, "\t'o%u' u ($9<0.001?$2:1/0):4 not w d,\\\n", i + 1);
+	fprintf (file, "\t'o%u' u ($9<0.001?$2:1/0):4 not w d\n", i + 1);
+	fprintf (file, "set out 'o1-2.eps'\n");
 	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
 	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d,\\\n",
+	  fprintf (file, "\t'o%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d,\\\n",
 			   i + 1);
-	fprintf (file, "\t'%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d\n", i + 1);
-	fprintf (file, "set out '2-3.eps'\n");
-	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
-	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d,\\\n",
+	fprintf (file, "\t'o%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d\n",
 			   i + 1);
-	fprintf (file, "\t'%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d\n", i + 1);
-	fprintf (file, "set out '3-4.eps'\n");
+	fprintf (file, "set out 'o2-3.eps'\n");
 	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
 	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d,\\\n",
+	  fprintf (file, "\t'o%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d,\\\n",
 			   i + 1);
-	fprintf (file, "\t'%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d\n", i + 1);
-	fprintf (file, "set out '4-5.eps'\n");
-	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
-	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d,\\\n",
+	fprintf (file, "\t'o%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d\n",
 			   i + 1);
-	fprintf (file, "\t'%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d\n", i + 1);
-	fprintf (file, "set out '5-.eps'\n");
+	fprintf (file, "set out 'o3-4.eps'\n");
 	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
 	for (i = 0; i < 2457; ++i)
-	  fprintf (file, "\t'%u' u ($9<0.005?1/0:$2):4 not w d,\\\n", i + 1);
-	fprintf (file, "\t'%u' u ($9<0.005?1/0:$2):4 not w d\n", i + 1);
+	  fprintf (file, "\t'o%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t'o%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 'o4-5.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t'o%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t'o%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 'o5-.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t'o%u' u ($9<0.005?1/0:$2):4 not w d,\\\n", i + 1);
+	fprintf (file, "\t'o%u' u ($9<0.005?1/0:$2):4 not w d\n", i + 1);
+	fprintf (file, "set out 's0-1.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.001?$2:1/0):4 not w d,\\\n", i + 1);
+	fprintf (file, "\t's%u' u ($9<0.001?$2:1/0):4 not w d\n", i + 1);
+	fprintf (file, "set out 's1-2.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t's%u' u ($9<0.001?1/0:$9<0.002?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 's2-3.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t's%u' u ($9<0.002?1/0:$9<0.003?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 's3-4.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t's%u' u ($9<0.003?1/0:$9<0.004?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 's4-5.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d,\\\n",
+			   i + 1);
+	fprintf (file, "\t's%u' u ($9<0.004?1/0:$9<0.005?$2:1/0):4 not w d\n",
+			   i + 1);
+	fprintf (file, "set out 's5-.eps'\n");
+	fprintf (file, "plot [0:][-1.35:3] f(x) t'Main jet' w l lt 2,\\\n");
+	for (i = 0; i < 2457; ++i)
+	  fprintf (file, "\t's%u' u ($9<0.005?1/0:$2):4 not w d,\\\n", i + 1);
+	fprintf (file, "\t's%u' u ($9<0.005?1/0:$2):4 not w d\n", i + 1);
 	fclose (file);	
 }
